dd7288d5d736520a3b22b1b7c179d42af3462633
tramtran0497 <tramtran0497@gmail.com>
2022-01-03 13:03:18 +0200

    Taking some descriptions after praticing 10 projects

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ReactProject
-Tram Tran is making this project
+There are 10 React practice projects (Laerning from youtuber account: `FreeCodeCamp.org`)
+
+All CSS file is copied from guider github.
+
+## What did learn from 10 projects?
+How to use components and get familiar with props fields, especially how to bring props through parent components to child components.
+
+The way to set up a project which is coded by `React` App.
+
+Understanding workflow of using `React` to create a project.
+
+Get some tools, libraries as well as `Material UI/Core`.
+
+Learning how to take data inside each componets, reusing components.
+
+## Comparing between using traditional way and react app to build a project after practise 10 projects?
+| Traditional way: HTML, CSS and JS file | React app |
+|----------------------------------------|-----------|
+|No reused |Reusing conponents: save time|
+|No supports |Many supports from libraries|
+|Need much time|Need less time to built almost a simple project|
+|Hard to read code| Readable coding|
--- birthdayreminder/src/data.js
+++ birthdayreminder/src/data.js
@@ -3,24 +3,12 @@ export default [
         id: 1,
         name: 'Vu Le',
         age: 26,
-        image: "https://scontent.fqlf1-1.fna.fbcdn.net/v/t1.6435-9/226238135_4270228459761885_1753779207361461408_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UmbT3iXLEdoAX_FuPFw&_nc_ht=scontent.fqlf1-1.fna&oh=ab0c48cff8dba568f20c962806762486&oe=61A8D9E6"
+        image: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
     },
     {
         id: 2,
         name: 'Tram Tran',
         age: 24,
-        image: "https://scontent.fqlf1-1.fna.fbcdn.net/v/t1.6435-9/250113638_1553687554970520_1798386211127502033_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=o6msz_TmHaQAX_wSv6t&_nc_ht=scontent.fqlf1-1.fna&oh=69ef5dea0abcbcbe0a89d3f485059dad&oe=61A836D0"
-    },
-    {
-        id: 3,
-        name: 'Minh Khang Le',
-        age: 7,
-        image: "https://scontent.fqlf1-1.fna.fbcdn.net/v/t39.30808-1/s480x480/244684344_107708265018273_6960967554014093116_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=7206a8&_nc_ohc=ji8kF4aHNHoAX82KvvW&_nc_ht=scontent.fqlf1-1.fna&oh=6da0988f4f6988e84fcca1c06d138705&oe=618793DD"
-    },
-    {
-        id: 4,
-        name: 'Ngoc Lien',
-        age: 32,
-        image: "https://scontent.fqlf1-1.fna.fbcdn.net/v/t39.30808-6/245115770_2958009557798459_6217619252961731188_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=CgLUr3JMnaEAX87Veyo&_nc_ht=scontent.fqlf1-1.fna&oh=6f10bb65835a85f7314d8a280440c920&oe=6187DEEC"
-    },
+        image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }
 ]
